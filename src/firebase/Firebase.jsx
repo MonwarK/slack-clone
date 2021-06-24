320223c6ca5f3c5656b34e51c2587ddea952e23f
Monwar Khan <72820069+MonwarK@users.noreply.github.com>
2021-06-24 14:08:07 +0100

    Update Firebase.jsx

--- src/firebase/Firebase.jsx
+++ src/firebase/Firebase.jsx
@@ -3,12 +3,7 @@ import "firebase/auth"
 import "firebase/firestore"
 
 const firebaseConfig = {
-    apiKey: "AIzaSyARTILtzGCuuSpAurnBNO2e7u9LDpoL7R8",
-    authDomain: "slack-clone-1e907.firebaseapp.com",
-    projectId: "slack-clone-1e907",
-    storageBucket: "slack-clone-1e907.appspot.com",
-    messagingSenderId: "105513734378",
-    appId: "1:105513734378:web:c8ae3afe359aa2d6f30793"
+    
 };
 
 Firebase.initializeApp(firebaseConfig);
